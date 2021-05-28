a78b83f729e7083b815bde470287dcacde4684db
akashdoppalapudi <akashdoppalapudi2001@gmail.com>
2021-05-28 11:50:57 +0530

    "linked lists"

--- linkedList.py
+++ linkedList.py
@@ -1,15 +1,24 @@
 class Node:
-    def __init__(self, val, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
     def formLink(self, next):
         self.next = next
 
+    def display(self):
+        node = self
+        while(node!=None):
+            print(node.val, "--> ", end="")
+            node = node.next
+        print(None)
+
+    def convert(self, l):
+        nodes = []
+        nodes.append(Node(l[0]))
+        for i in range(1,len(l)):
+            nodes.append(Node(l[i]))
+            nodes[i-1].formLink(nodes[i])
+
+        return nodes[0]
     
-
-node1 = Node(5)
-node2 = Node(6)
-node1.formLink(node2)
-
-print(node1.next.next)
