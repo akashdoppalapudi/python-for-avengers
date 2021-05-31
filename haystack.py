04537bb61550481ec0ac35001b115a1fbbfc3ed7
akashdoppalapudi <akashdoppalapudi2001@gmail.com>
2021-05-31 11:57:31 +0530

    haystack

---
+++ haystack.py
@@ -0,0 +1,7 @@
+"""
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+"""
+
+haystack = input()
+needle = input()
+print(haystack.find(needle))
